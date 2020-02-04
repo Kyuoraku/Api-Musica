12cc218750dba7ca22ec3fe4143b70dda9498214
Kyuoraku <nico.99.godoy@gmail.com>
2020-02-04 11:19:17 -0300

    Listo el servidor para levantarse. Falta configurar las rutas

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let mongoose = require('mongoose')
+let app = require ('./app')
+const PORT = process.env.PORT || 3977
+
+mongoose.connect('mongodb://localhost:27017/curso_mean', (err,res) =>{
+
+    if (err){
+        throw err
+    }else{
+        console.log('La base de datos está corriendo correctamente')
+
+        app.listen(PORT, function(){
+
+            console.log('Servidor del API Rest de Musica escuchando en http://localhost:',PORT)
+
+        })
+    }
+
+})
